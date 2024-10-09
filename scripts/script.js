db42be7cfed208d0bc6dd3f90b7f22fb969c9df6
azanra <112457939+azanra@users.noreply.github.com>
2024-10-09 08:32:04 +0700

    Add function to create array and task object

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+function createArrayTask() {
+    let arrayTask = [];
+    return arrayTask;
+}
+
+function createObjTask(id, detail, status) {
+    this.taskId = id;
+    this.taskDetail = detail;
+    this.taskStatus = status;
+}
+
+function main() {
+
+}
+
+main();
