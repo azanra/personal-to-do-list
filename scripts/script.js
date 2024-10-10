854cd009776a959c72183320e557d3c8329c1880
azanra <112457939+azanra@users.noreply.github.com>
2024-10-10 13:40:07 +0700

    Add function get user input and create obj for it

--- scripts/script.js
+++ scripts/script.js
@@ -9,8 +9,34 @@ function createObjTask(id, detail, status) {
     this.taskStatus = status;
 }
 
+function getUserInput() {
+    let userInput = [];
+    userInput = prompt("Input in format: id,detail,status");
+    return userInput;
+}
+
+function splitInput(inputStr) {
+    let arrStr = inputStr.split(",");
+    console.log(arrStr);
+    return arrStr;
+}
+
+function assignInput(inputArr) {
+    inputId = inputArr[0];
+    inputDetail = inputArr[1];
+    inputStatus = inputArr[2];
+    
+    const taskObj = new createObjTask(inputId, inputDetail, inputStatus);
+    return taskObj;
+}
+
 function main() {
 
+    let userInputArr = (splitInput(getUserInput()));
+    console.log(userInputArr);
+    
+    const newTaskObj = assignInput(userInputArr);
+    console.log(newTaskObj);
 }
 
 main();
